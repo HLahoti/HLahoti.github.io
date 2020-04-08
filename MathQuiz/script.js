5c8ab73df7e22cdae62549801f32d29f344084da
MorseBoy <laidtorest.harsh@gmail.com>
2020-04-08 14:33:29 +0530

    Redesigned settings bar for math quiz webpage

--- MathQuiz/script.js
+++ MathQuiz/script.js
@@ -1,32 +1,55 @@
-let highscore_field,
-	currentscore_field,
-	next_btn,
-	submit_btn,
-	answer_field,
-	num1_field,
-	num2_field,
-	operator_field,
-	output_field,
-	toggle,
-	bar,
-	bar2,
-	isBarVisible = false,
+const idGetter = (id) => document.getElementById(id);
+const classGetter = (className) => document.getElementsByClassName(className);
+
+const highscore_field = idGetter("highscore_text"),
+	currentscore_field = idGetter("currentscore_text"),
+	num1_field = idGetter("num_1"),
+	num2_field = idGetter("num_2"),
+	operator_field = idGetter("operator"),
+	next_btn = idGetter("next"),
+	submit_btn = idGetter("submit"),
+	answer_field = idGetter("answer_field"),
+	output_field = idGetter("check"),
+	toggle = idGetter("toggle"),
+	toggle2 = idGetter("toggle2"),
+	bar = idGetter("bar"),
+	bar2 = idGetter("bar2"),
+	add = idGetter("plus"),
+	subtract = idGetter("minus"),
+	multiply = idGetter("multiply"),
+	divide = idGetter("divide"),
+	add_max = idGetter("addmax"),
+	add_min = idGetter("addmin"),
+	subtract_max = idGetter("minusmax"),
+	subtract_min = idGetter("minusmin"),
+	multiply_max = idGetter("multiplymax"),
+	multiply_min = idGetter("multiplymin"),
+	divide_max = idGetter("dividemax"),
+	divide_min = idGetter("dividemin");
+
+let isBarVisible = false,
 	isSettingsBarVisible = false,
 	colorSchemes = {
 		black: {
 			bg: "black",
 			text: "white",
 			accents: "white",
+			navbarSide: "5vw solid rgba(225,225,225, 0.87)",
+			navbarHoverSide: "5vw solid rgba(225,225,225, 0.0)",
 		},
 		sepia: {
 			bg: "antiquewhite",
 			text: "black",
 			accents: "black",
+			navbarSide: "5vw solid rgba(47, 47, 47, 0.87)",
+			navbarHoverSide: "5vw solid rgba(47, 47, 47, 0.0)",
 		},
 		white: {
 			bg: "white",
 			text: "black",
 			accents: "black",
+			navbarSide: "5vw solid rgba(47, 47, 47, 0.87)",
+			navbarHoverSide: "5vw solid rgba(47, 47, 47, 0.0)",
 		},
 	},
 	current_color_scheme = "sepia",
@@ -36,18 +59,6 @@ let highscore_field,
 		multiply: true,
 		divide: true,
 	},
-	add,
-	add_max,
-	add_min,
-	subtract,
-	subtract_max,
-	suubtract_min,
-	multiply,
-	multiply_max,
-	multiply_min,
-	divide,
-	divide_max,
-	divide_min,
 	operators_available = ["+", "-", "/", "x"],
 	plus = { maxnum: 600, minnum: 40 },
 	minus = { maxnum: 600, minnum: 40 },
@@ -56,78 +67,90 @@ let highscore_field,
 	divide_1st = { maxnum: 600, minnum: 10 },
 	divide_2nd = { maxnum: 30, minnum: 2 };
 
-const idGetter = (id) => document.getElementById(id);
-const classGetter = (className) => document.getElementsByClassName(className);
-
-window.addEventListener("load", function () {
-	//
-	highscore_field = idGetter("highscore_text");
-	currentscore_field = idGetter("currentscore_text");
-	num1_field = idGetter("num_1");
-	num2_field = idGetter("num_2");
-	operator_field = idGetter("operator");
-	next_btn = idGetter("next");
-	submit_btn = idGetter("submit");
-	answer_field = idGetter("answer_field");
-	output_field = idGetter("check");
-	toggle = idGetter("toggle");
-	toggle2 = idGetter("toggle2");
-	bar = idGetter("bar");
-	bar2 = idGetter("bar2");
-	add = idGetter("plus");
-	subtract = idGetter("minus");
-	multiply = idGetter("multiply");
-	divide = idGetter("divide");
-	add_max = idGetter("addmax");
-	add_min = idGetter("addmin");
-	subtract_max = idGetter("minusmax");
-	subtract_min = idGetter("minusmin");
-	multiply_max = idGetter("multiplymax");
-	multiply_min = idGetter("multiplymin");
-	divide_max = idGetter("dividemax");
-	divide_min = idGetter("dividemin");
-	//
-	colorSchemeSetter(current_color_scheme);
-	operator_editor();
-	//
-	next_btn.addEventListener("click", next_func);
-	submit_btn.addEventListener("click", submit_func);
-	toggle.addEventListener("click", toggleBar);
-	toggle2.addEventListener("click", toggleBar);
-	const checkboxes = classGetter("checkbox");
-	//
-	add_max.value = "";
-	add_min.value = "";
-	subtract_max.value = "";
-	subtract_min.value = "";
-	multiply_max.value = "";
-	multiply_min.value = "";
-	divide_max.value = "";
-	divide_min.value = "";
-	//
-	add_max.addEventListener("change", addRangeEditor);
-	add_min.addEventListener("change", addRangeEditor);
-	subtract_max.addEventListener("change", subtractRangeEditor);
-	subtract_min.addEventListener("change", subtractRangeEditor);
-	multiply_max.addEventListener("change", multiplyRangeEditor);
-	multiply_min.addEventListener("change", multiplyRangeEditor);
-	divide_max.addEventListener("change", divideRangeEditor);
-	divide_min.addEventListener("change", divideRangeEditor);
-	for (let i = 0; i < checkboxes.length; i++) {
-		checkboxes[i].addEventListener("click", checkHandler);
+const colorSchemeSetter = (color) => {
+	current_color_scheme = color;
+	const container = idGetter("mega-container");
+	container.style.backgroundColor = colorSchemes[color].bg;
+	container.style.color = colorSchemes[color].text;
+	answer_field.style.borderTop = `4px solid ${colorSchemes[color].accents}`;
+	answer_field.style.borderBottom = `4px solid ${colorSchemes[color].accents}`;
+	const btns = classGetter("btn");
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].style.border = `3px solid ${colorSchemes[color].accents}`;
 	}
-	//
-	answer_field.value = "";
-	submit_btn.style.opacity = 0;
-	submit_btn.style.pointerEvents = "none";
-	add.checked = operatorsSelected.plus;
-	subtract.checked = operatorsSelected.minus;
-	multiply.checked = operatorsSelected.multiply;
-	divide.checked = operatorsSelected.divide;
-});
+	const toggle_sciwtches = classGetter("toggle");
+	for (let i = 0; i < toggle_sciwtches.length; i++) {
+		let colour_bg = colorSchemes[color].bg === "black" ? "white" : "black",
+			colour_text =
+				colorSchemes[color].text === "black" ? "white" : "black";
+		toggle_sciwtches[i].style.backgroundColor = colour_bg;
+		toggle_sciwtches[i].style.color = colour_text;
+	}
+	answer_field.style.color = colorSchemes[color].text;
+	const bar_elements = classGetter("bar_element");
+	for (let i = 0; i < bar_elements.length; i++) {
+		bar_elements[
+			i
+		].style.border = `3px solid ${colorSchemes[color].accents}`;
+	}
+	const colors = classGetter("color");
+	for (let i = 0; i < colors.length; i++) {
+		colors[i].style.border = `1px solid ${colorSchemes[color].accents}`;
+	}
+	let inputs = classGetter("maxnum");
+	for (let i = 0; i < inputs.length; i++) {
+		inputs[i].style.border = `1px solid ${colorSchemes[color].accents}`;
+		inputs[i].style.color = colorSchemes[color].text;
+	}
+	inputs = classGetter("minnum");
+	for (let i = 0; i < inputs.length; i++) {
+		inputs[i].style.border = `1px solid ${colorSchemes[color].accents}`;
+		inputs[i].style.color = colorSchemes[color].text;
+	}
+	const navbar = classGetter("settings");
+	for (let i = 0; i < navbar.length; i++) {
+		navbar[i].style.borderRight = `${colorSchemes[color].navbarSide}`;
+		navbar[i].style.backgroundColor = colorSchemes[color].bg;
+	}
+};
 
-let highscore,
-	currentscore,
+setInterval(() => {
+	if (screen.width >= 500) {
+		for (obj in colorSchemes) {
+			colorSchemes[obj].navbarSide = "none";
+			colorSchemes[obj].navbarHoverSide = "none";
+		}
+		colorSchemeSetter(current_color_scheme);
+	} else {
+		colorSchemes.black.navbarSide = "5vw solid rgba(225,225,225, 0.87)";
+		colorSchemes.black.navbaHoverSide = "5vw solid rgba(225,225,225, 0.0)";
+		colorSchemes.sepia.navbarSide = "5vw solid rgba(47, 47, 47, 0.87)";
+		colorSchemes.sepia.navbaHoverSide = "5vw solid rgba(47, 47, 47, 0.0)";
+		colorSchemes.black.navbarSide = "5vw solid rgba(47, 47, 47, 0.87)";
+		colorSchemes.black.navbaHoverSide = "5vw solid rgba(47, 47, 47, 0.0)";
+		colorSchemeSetter(current_color_scheme);
+	}
+}, 1000);
+
+const operator_editor = () => {
+	let list = [];
+	if (operatorsSelected.plus) {
+		list.push("+");
+	}
+	if (operatorsSelected.minus) {
+		list.push("-");
+	}
+	if (operatorsSelected.multiply) {
+		list.push("x");
+	}
+	if (operatorsSelected.divide) {
+		list.push("/");
+	}
+	operators_available = list;
+};
+
+let highscore = 0,
+	currentscore = 0,
 	num1,
 	num2,
 	operator,
@@ -165,23 +188,6 @@ const checkHandler = (event) => {
 	operator_editor();
 };
 
-const operator_editor = () => {
-	let list = [];
-	if (operatorsSelected.plus) {
-		list.push("+");
-	}
-	if (operatorsSelected.minus) {
-		list.push("-");
-	}
-	if (operatorsSelected.multiply) {
-		list.push("x");
-	}
-	if (operatorsSelected.divide) {
-		list.push("/");
-	}
-	operators_available = list;
-};
-
 const start = () => {
 	if (started == false) {
 		highscore = 0;
@@ -206,13 +212,13 @@ const spaceAdder = (number) => {
 const main = () => {
 	let operator_index = Math.floor(Math.random() * operators_available.length);
 	operator = operators_available[operator_index];
-	if (operator_index == 0) {
+	if (operator == "+") {
 		plus_func();
-	} else if (operator_index == 1) {
+	} else if (operator == "-") {
 		minus_func();
-	} else if (operator_index == 2) {
+	} else if (operator == "x") {
 		multiply_func();
-	} else if (operator_index == 3) {
+	} else if (operator == "/") {
 		divide_func();
 	}
 	num1_field.innerHTML = spaceAdder(num1);
@@ -224,11 +230,20 @@ const main = () => {
 	answer_field.style.borderBottom = `4px solid ${colorSchemes[current_color_scheme].accents}`;
 };
 
+const switchero = () => {
+	if (num2 > num1) {
+		num1 = num2 + num1;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+	}
+};
+
 const plus_func = () => {
 	let maxint = plus.maxnum,
 		minint = plus.minnum;
 	num1 = Math.floor(Math.random() * (maxint - minint) + minint);
 	num2 = Math.floor(Math.random() * (maxint - minint) + minint);
+	switchero();
 	checkedanswer = num1 + num2;
 	console.log(`${num1} + ${num2} = ${checkedanswer}`);
 };
@@ -238,11 +253,7 @@ const minus_func = () => {
 	num1 = Math.floor(Math.random() * (maxint - minint + 1) + minint);
 	num2 = Math.floor(Math.random() * (maxint - minint + 1) + minint);
 	num2 = num2 == num1 ? num2 - 1 : num2;
-	if (num2 > num1) {
-		let x = num2;
-		num2 = num1;
-		num1 = x;
-	}
+	switchero();
 	checkedanswer = num1 - num2;
 	console.log(`${num1} - ${num2} = ${checkedanswer}`);
 };
@@ -255,14 +266,7 @@ const divide_func = () => {
 	while (true != false) {
 		num1 = Math.floor(Math.random() * (maxint1 - minint1 + 1) + minint1);
 		num2 = Math.floor(Math.random() * (maxint2 - minint2 + 1) + minint2);
-		if (num2 > num1 || num1 % num2 != 0) {
-			num1 = Math.floor(
-				Math.random() * (maxint1 - minint1 + 1) + minint1
-			);
-			num2 = Math.floor(
-				Math.random() * (maxint2 - minint2 + 1) + minint2
-			);
-		} else {
+		if (num2 < num1 && num1 % num2 === 0) {
 			break;
 		}
 	}
@@ -276,6 +280,7 @@ const multiply_func = () => {
 		minint2 = multiply_2nd.minnum;
 	num1 = Math.floor(Math.random() * (maxint1 - minint1 + 1) + minint1);
 	num2 = Math.floor(Math.random() * (maxint2 - minint2 + 1) + minint2);
+	switchero();
 	checkedanswer = num1 * num2;
 	console.log(`${num1} x ${num2} = ${checkedanswer}`);
 };
@@ -348,44 +353,60 @@ const toggleBar = (event) => {
 	}
 };
 
-const colorSchemeSetter = (color) => {
-	current_color_scheme = color;
-	const container = idGetter("mega-container");
-	container.style.backgroundColor = colorSchemes[color].bg;
-	container.style.color = colorSchemes[color].text;
-	answer_field.style.borderTop = `4px solid ${colorSchemes[color].accents}`;
-	answer_field.style.borderBottom = `4px solid ${colorSchemes[color].accents}`;
-	const btns = classGetter("btn");
-	for (let i = 0; i < btns.length; i++) {
-		btns[i].style.border = `3px solid ${colorSchemes[color].accents}`;
+const preset = () => {
+	colorSchemeSetter(current_color_scheme);
+	operator_editor();
+
+	next_btn.addEventListener("click", next_func);
+	submit_btn.addEventListener("click", submit_func);
+	toggle.addEventListener("click", toggleBar);
+	toggle2.addEventListener("click", toggleBar);
+	const checkboxes = classGetter("checkbox");
+
+	add_max.value = "";
+	add_min.value = "";
+	subtract_max.value = "";
+	subtract_min.value = "";
+	multiply_max.value = "";
+	multiply_min.value = "";
+	divide_max.value = "";
+	divide_min.value = "";
+	//
+	add_max.addEventListener("change", addRangeEditor);
+	add_min.addEventListener("change", addRangeEditor);
+	subtract_max.addEventListener("change", subtractRangeEditor);
+	subtract_min.addEventListener("change", subtractRangeEditor);
+	multiply_max.addEventListener("change", multiplyRangeEditor);
+	multiply_min.addEventListener("change", multiplyRangeEditor);
+	divide_max.addEventListener("change", divideRangeEditor);
+	divide_min.addEventListener("change", divideRangeEditor);
+	for (let i = 0; i < checkboxes.length; i++) {
+		checkboxes[i].addEventListener("click", checkHandler);
 	}
-	const toggle_sciwtches = classGetter("toggle");
-	for (let i = 0; i < toggle_sciwtches.length; i++) {
-		let colour_bg = colorSchemes[color].bg === "black" ? "white" : "black",
-			colour_text =
-				colorSchemes[color].text === "black" ? "white" : "black";
-		toggle_sciwtches[i].style.backgroundColor = colour_bg;
-		toggle_sciwtches[i].style.color = colour_text;
+	let toggles = classGetter("settings-container");
+	for (let i = 0; i < toggles.length; i++) {
+		toggles[i].onmouseover = () => {
+			nav.style.borderRight = `${colorSchemes[current_color_scheme].navbarHoverSide}`;
+		};
+		toggles[i].addEventListener("mouseout", () => {
+			nav.style.borderRight = `${colorSchemes[current_color_scheme].navbarHoverSide}`;
+		});
 	}
-	answer_field.style.color = colorSchemes[color].text;
-	const bar_elements = classGetter("bar_element");
-	for (let i = 0; i < bar_elements.length; i++) {
-		bar_elements[
-			i
-		].style.border = `3px solid ${colorSchemes[color].accents}`;
-	}
-	const colors = classGetter("color");
-	for (let i = 0; i < colors.length; i++) {
-		colors[i].style.border = `1px solid ${colorSchemes[color].accents}`;
-	}
-	let inputs = classGetter("maxnum");
-	for (let i = 0; i < inputs.length; i++) {
-		inputs[i].style.border = `1px solid ${colorSchemes[color].accents}`;
-		inputs[i].style.color = colorSchemes[color].text;
-	}
-	inputs = classGetter("minnum");
-	for (let i = 0; i < inputs.length; i++) {
-		inputs[i].style.border = `1px solid ${colorSchemes[color].accents}`;
-		inputs[i].style.color = colorSchemes[color].text;
-	}
+	let nav = idGetter("nav");
+	nav.onmouseover = () => {
+		nav.style.borderRight = `${colorSchemes[current_color_scheme].navbarHoverSide}`;
+	};
+	nav.addEventListener("mouseout", () => {
+		nav.style.borderRight = `${colorSchemes[current_color_scheme].navbarSide}`;
+	});
+	//
+	answer_field.value = "";
+	submit_btn.style.opacity = 0;
+	submit_btn.style.pointerEvents = "none";
+	add.checked = operatorsSelected.plus;
+	subtract.checked = operatorsSelected.minus;
+	multiply.checked = operatorsSelected.multiply;
+	divide.checked = operatorsSelected.divide;
 };
+
+preset();
